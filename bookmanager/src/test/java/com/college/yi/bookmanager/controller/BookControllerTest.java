7cb6609be44853ef9ff059fce97f35da9f388352
yr3a2841w <yuusiruto3@gmail.com>
2025-08-02 11:25:16 +0900

    4週目課題提出_松尾　雄志

--- bookmanager/src/test/java/com/college/yi/bookmanager/controller/BookControllerTest.java
+++ bookmanager/src/test/java/com/college/yi/bookmanager/controller/BookControllerTest.java
@@ -1,30 +1,40 @@
 package com.college.yi.bookmanager.controller;
 
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.college.yi.bookmanager.model.Book;
 import com.college.yi.bookmanager.service.BookService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
+@AutoConfigureMockMvc(addFilters = false)
 @WebMvcTest(BookController.class)
 public class BookControllerTest {
 
+	@Autowired
 	private MockMvc mockMvc;
-	
+
 	@MockBean
 	private BookService bookService;
 	
 	private Book book;
+	
+	@Autowired
+	private ObjectMapper objectMapper;
 	
 	@BeforeEach
 	void setUp(){
@@ -42,6 +52,52 @@ public class BookControllerTest {
 	@Test
 	void testGetBooks() throws Exception{
 		when(bookService.getBooks()).thenReturn(List.of(book));
+		
+		mockMvc.perform(get("/api/books"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.length()").value(1))
+				.andExpect(jsonPath("$[0].title").value("タイトル"));
+		
+		verify(bookService).getBooks();
+	}
+	
+	@Test
+	void testCreateBooks() throws Exception{
+		when(bookService.createBook(any(Book.class))).thenReturn(book);
+		
+		mockMvc.perform(post("/api/books")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(book)))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.title").value("タイトル"));
+		
+		verify(bookService).createBook(any(Book.class));
+		
+	}
+	
+	@Test
+	void testUpdateBooks() throws Exception{
+		when(bookService.updateBook(eq(1),any(Book.class))).thenReturn(book);
+		
+		mockMvc.perform(put("/api/books/1")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(book)))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.title").value("タイトル"));
+		
+		verify(bookService).updateBook(eq(1),any(Book.class));
+		
+	}
+	
+	@Test
+	void testDeleteBooks() throws Exception{
+		doNothing().when(bookService).deleteBook(1);
+		
+		mockMvc.perform(delete("/api/books/1"))
+				.andExpect(status().isOk());
+				
+		verify(bookService).deleteBook(1);
+		
 	}
 	
 }
